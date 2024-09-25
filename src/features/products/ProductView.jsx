42afadbf732a96755dbd3b3071445ac87b6e256f
Mostary Jahan <mostaryjahan01@gmail.com>
2024-09-25 22:27:53 +0600

    added slice

--- src/features/products/ProductView.jsx
+++ src/features/products/ProductView.jsx
@@ -3,8 +3,9 @@ const ProductView = () => {
     return (
         <div>
             <h2>List of products</h2>
+            <h3>You can see all of our products here</h3>
         </div>
     );
 };
 
-export default ProductView;
+export default ProductView; 
